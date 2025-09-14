800c3c5374754bea06dc2a0863078ebda11eebbd
oclaudiodev <7claudiosouza@gmail.com>
2025-09-14 16:58:20 -0300

    atualizado

--- frontend/src/pages/CadastrarAgenda.jsx
+++ frontend/src/pages/CadastrarAgenda.jsx
@@ -1,49 +1,85 @@
 import { useState } from "react";
 import api from "../api";
 import { Link } from "react-router";
-import './cadastro.scss'
-
+import './cadastro.scss';
 
 export default function ListarAgenda() {
-    const [titulo, setTitulo] = useState('')
-    const [descricao, setDescricao] = useState('')
-    const [dataEvento, setDataEvento] = useState('')
+    const [titulo, setTitulo] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [dataEvento, setDataEvento] = useState('');
 
     async function criarEvento() {
         await api.post('/eventos', {
-            "titulo": titulo,
-            "descricao": descricao,
-            "dataevento": dataEvento
+            titulo,
+            descricao,
+            dataevento: dataEvento
         })
-            .then(() => alert('Evento criado!'))
-            .catch(e => alert(e.response.data.erro))
+        .then(() => alert('Evento criado!'))
+        .catch(e => alert(e.response?.data?.erro || "Erro ao criar evento."));
     }
 
     return (
-        <div>
-            <h1>Cadastrar Evento</h1>
-            
-            <div className="formulario">
-                <div>
-                    <label>Titulo</label>
-                    <input value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+        <div className="page">
+            <div className="conteudo">
+                <div className="cabecalho">
+                    <img className="logo" src="/src/assets/images/logoP.png" height="90px" alt="logoP" />
+                    <h1>Agenda Frei</h1>
+                    <div className="navegacao">
+                        <Link to ={'/listar'}><h1>Inicio</h1></Link>
+                        <Link to ={'/'}><h1>Cadastro</h1></Link>
+                    </div>
                 </div>
-                <br />
-                <div>
-                    <label>Descricao</label>
-                    <input value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+
+                <h1 className="espaco">Cadastrar Evento</h1>
+
+                <div className="formulario">
+                    <div>
+                        <label>Título</label>
+                        <br />
+                        <input value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+                    </div>
+                    <br />
+                    <div>
+                        <label>Descrição</label>
+                        <br />
+                        <input value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+                    </div>
+                    <br />
+                    <div>
+                        <label>Data do Evento</label>
+                        <br />
+                        <input value={dataEvento} onChange={(e) => setDataEvento(e.target.value)} />
+                    </div>
+                    <br />
+                    <div className="butoes">
+                        <p>id:</p>
+                        <button onClick={criarEvento}>Criar</button>
+                        <button>Salvar</button>
+                    </div>
+                    <br />
+                    <Link to="/listar">Veja todos os Eventos</Link>
                 </div>
-                <br />
-                <label>Data do Evento</label>
-                <input value={dataEvento} onChange={(e) => setDataEvento(e.target.value)} />
-                <br />
-                <button onClick={criarEvento}>Criar</button>
-                
-                <br></br>
-                <Link to={'/listar'}>Veja todos os Eventos</Link>
-                
             </div>
 
+            <footer>
+                <div className="rodape">
+                    <img className="logo" src="/src/assets/images/logoB.png" alt="logoB.png" />
+                    <div>
+                        <h3>Contato</h3>
+                        <p>(11)95100-8673</p>
+                        <p>7claudiosouza@gmail.com</p>
+                    </div>
+                    <div>
+                        <h3>Desenvolvido Por</h3>
+                        <p>José Claudio de Souza Silva</p>
+                    </div>
+                    <div>
+                        <h3>Turma</h3>
+                        <p>Informática - A</p>
+                    </div>
+                </div>
+                <h3 className="fim">2025 @ Instituto Nossa Senhora de Fátima</h3>
+            </footer>
         </div>
     );
 }
--- frontend/src/pages/ListarAgenda.jsx
+++ frontend/src/pages/ListarAgenda.jsx
@@ -1,47 +1,48 @@
 import { useState } from "react";
 import api from "../api";
 import { Link } from "react-router";
-import './listar.scss'
+import './listar.scss';
 
 export default function ListarAgenda() {
-    const [agenda, setAgenda] = useState([])
+    const [agenda, setAgenda] = useState([]);
 
     async function carregarAgendaAxios() {
         await api.get('/eventos')
-            .then(resposta => setAgenda(resposta.data))
+            .then(resposta => setAgenda(resposta.data));
     }
 
-
     return (
-        <div>
-            <div className="cabecalho">
-                <img className="logo" src="/src/assets/images/logoP.png" height='90px' alt="logoP" />
-                <h1>Agenda Frei</h1>
-                <div className="navegacao">
-                    <h1>Inicio</h1>
-                    <h1>Cadastro</h1>
+        <div className="container">
+            <div className="conteudo">
+                <div className="cabecalho">
+                    <img className="logo" src="/src/assets/images/logoP.png" height='90px' alt="logoP" />
+                    <h1>Agenda Frei</h1>
+                    <div className="navegacao">
+                        <Link to ={'/listar'}><h1>Inicio</h1></Link>
+                        <Link to ={'/'}><h1>Cadastro</h1></Link>
+                    </div>
                 </div>
-            </div>
-            <h1>Listar Agenda - CRUD</h1>
 
-            <main>
-                <button className="butao" onClick={carregarAgendaAxios}>Consultar</button>
-                <div className="cardM">
-                    {
-                        agenda.map(evento => {
-                            return <div className="card">
-                                <h2>{evento.titulo}</h2>
-                                <p>{evento.descricao}</p>
-                                <h3>{evento.dataevento}</h3>
-                            </div>
-                        })
-                    }
-                </div>
-                <br></br>
-                <Link to={'/'}>Crie um novo evento</Link>
-                <br />
-                <br />
-            </main>
+                <h1>Listar Agenda - CRUD</h1>
+
+                <main>
+                    <button className="butao" onClick={carregarAgendaAxios}>Consultar</button>
+                    <div className="cardM">
+                        {
+                            agenda.map(evento => (
+                                <div className="card" key={evento.id}>
+                                    <h2>{evento.titulo}</h2>
+                                    <p>{evento.descricao}</p>
+                                    <h3>{evento.dataevento}</h3>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <br />
+                    <Link to={'/'}>Crie um novo evento</Link>
+                    <br /><br />
+                </main>
+            </div>
 
             <footer>
                 <div className="rodape">
@@ -62,7 +63,6 @@ export default function ListarAgenda() {
                 </div>
                 <h3 className="fim">2025 @ Instituto Nossa Senhora de Fátima</h3>
             </footer>
-            
         </div>
     );
 }
